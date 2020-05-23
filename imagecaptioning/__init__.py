ff5a81a211b4ef64f32b35add94d78c7cadfa727
jjmachan <jamesjithin97@gmail.com>
2020-05-23 13:29:51 +0530

    doc string for imagecaptioner class

--- imagecaptioning/__init__.py
+++ imagecaptioning/__init__.py
@@ -1,3 +1,3 @@
 from .caption import ImageCaptioner
 
-__version__ = '0.1.0'
+__version__ = '0.1.6'
--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_desc = fh.read()
 setup(
-    name='src',
+    name='imagecaptioning-aeye',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.6',
     description='The Image Captioning module for the A-eye project.',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jjmachan/imagecaptioning-aeye',
     author='jjmachan',
+    authon_email='jamesjithin97@gmail.com',
     license='MIT',
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+                ],
+    python_requires='>=3.6',
 )
